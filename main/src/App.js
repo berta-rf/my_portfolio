46a3c468f560acfc52d0bb2974ea3759ce3e2d5a
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-03-03 12:53:13 +0000

    Add scss and create components files

---
+++ main/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Home from './components/Home';
+import ProjectGallery from './components/ProjectGallery';
+
+import 'App.scss';
+
+
+function App() {
+}
+
+export default App;
+
+
---
+++ main/src/components/ProjectGallery.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Project from './Project';
+
+
+const ProjectGallery = () => {
+
+
+}
+
+export default ProjectGallery;
