b91f5663289b59ce77042577b6e9c69c7dcc0995
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-03-03 14:15:54 +0000

    Add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
+    return(<>
+
+        <nav classNameName="navbar">
+            <h1>/berta-rf</h1>
+            <div classNameName="navLinks">
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Get in touch</Link>
+            </div>
+        </nav>
+
+    </>);
 
 }
 
